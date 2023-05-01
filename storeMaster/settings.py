424070ba695ebeee2f52029f80d97cbbcfecf2da
OmerBilgin21 <omrfrkblgn21@gmail.com>
2023-05-02 00:52:19 +0300

    fix: hide secret key with env variables

--- storeMaster/settings.py
+++ storeMaster/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y+s2i*e@13rj+ck#w)u=ql!ak=xo24s31%u4noklb*$b!)u@^p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -60,7 +64,7 @@ ROOT_URLCONF = 'storeMaster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'store-master-frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
